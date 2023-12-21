9181a43eca935d9507ba4b833542a384ae7fd2d0
lucas2003 <lucasatuincar1@gmail.com>
2023-12-20 23:32:29 -0500

    nombres de paginas

--- saludo.php
+++ saludo.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="keywords" content="Christmas, sale">
     <meta name="description" content="">
-    <title>Casa</title>
+    <title>ProyectoJoJoJo</title>
     <!-- Bootstrap CSS v5.2.1 -->
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
